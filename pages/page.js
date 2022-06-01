180ad2564f5f96af7cf285bfbdd38590cb996c28
tielefernandes <tielefernandes@gmail.com>
2022-06-01 15:49:45 -0300

    segundo cenário

--- pages/page.js
+++ pages/page.js
@@ -18,6 +18,18 @@ const ID_STATE_SELECT = `#id_state`;
 const ID_ZIPCODE_INPUT = `#postcode`;
 const ID_MOBILEPHONE_INPUT = `#phone_mobile`;
 const ID_REGISTRER_BUTTON = `#submitAccount`;
+const ID_SEARCH_INPUT = `#search_query_top`;
+const XPATH_SEARCH_BUTTON = `//form[@id='searchbox']/descendant::button`;
+const XPATH_CARDPRODUCT_A = `//a[@class='product_img_link']]`;
+const XPATH_ADDTOCART_BUTTON = `//span[text()='Add to cart']`;
+const XPATH_PROCEDTOCHECKOUT_BUTTON = `//span[contains(text(),'Proceed to checkout')]`;
+const XPATH_CONTINUESHOPPING_BUTTON = `//span[@title='Continue shopping']`;
+const XPATH_PRODUTO_DIV = `//div[@class='primary_block row']`;
+const XPATH_LISTICON_A = `//i[@class='icon-th-list']`;
+const XPATH_PROCEDTOCHECKOUT2_BUTTON = `//span[text()='Proceed to checkout']`;
+const ID_AGREETERMS_DIV = `#uniform-cgv`;
+const XPATH_CONFIRMORDER_SPAN = `//span[text()='I confirm my order']`;
+const MENSAGEMCONFIRMACAOPEDIDO = `//p[text()='Your order on My Store is complete.']`;
 
 module.exports = {
   acessarPaginaSignin() {
@@ -64,7 +76,47 @@ module.exports = {
     I.pressKey("K", "A", "N");
     I.pressKey("Enter");
     I.fillField(ID_ZIPCODE_INPUT, parametros.zipCode);
-    (parametros.additionalInformation) && I.fillField(ID_MOBILEPHONE_INPUT, parametros.mobilePhone);
+    parametros.additionalInformation &&
+      I.fillField(ID_MOBILEPHONE_INPUT, parametros.mobilePhone);
     I.click(ID_REGISTRER_BUTTON);
+  },
+
+  buscarProduto(parametros) {
+    I.fillField(ID_SEARCH_INPUT, parametros.product);
+    I.click(XPATH_SEARCH_BUTTON);
+    I.wait(3);
+    I.waitForText("Categories");
+    I.click(XPATH_LISTICON_A);
+  },
+
+  adicionarProdutoCarrinho() {
+    I.click(XPATH_ADDTOCART_BUTTON);
+    this.modalConfirmacaoProduto('Sim');
+  },
+
+  modalConfirmacaoProduto(fecharCarrinho) {
+    I.wait(4);
+    (fecharCarrinho == 'Sim') ? I.click(XPATH_PROCEDTOCHECKOUT_BUTTON) : I.click(XPATH_CONTINUESHOPPING_BUTTON);
+  },
+
+  finalizarPedido(){
+    I.click(XPATH_PROCEDTOCHECKOUT2_BUTTON);
+    I.click(XPATH_PROCEDTOCHECKOUT2_BUTTON);
+    I.checkOption(ID_AGREETERMS_DIV);
+    I.click(XPATH_PROCEDTOCHECKOUT2_BUTTON);
+    this.opcaoPagamento('Pay by check.');
+  },
+
+  /**
+   * função para escolher a forma de pagamento, quando invocada passar o parametro
+   * @param {*} opcao = 'Pay by bank wire' ou 'Pay by check.'
+   */
+  opcaoPagamento(opcao) {
+    let xpath_opcao_a = `//a[@title='${opcao}']`;
+    I.click(xpath_opcao_a);
+    I.click(XPATH_CONFIRMORDER_SPAN);
+    I.waitForVisible(MENSAGEMCONFIRMACAOPEDIDO);
   }
 };
+
+
--- tests/teste_tangerino_test.js
+++ tests/teste_tangerino_test.js
@@ -7,12 +7,22 @@ const lastName = faker.name.lastName();
 const address = faker.address.streetAddress();
 const email = faker.internet.email(firstName);
 
+const XPATH_HOME_SPAN = `//span[text() = ' Home' ]`;
+
 Scenario("cadastro de usuário com sucesso", ({ I, testPage }) => {
   I.amOnPage("http://automationpractice.com/index.php");
   testPage.acessarPaginaSignin();
   testPage.cadastroConta(parametros.dados, parametros.dados.emailValido);
-  testPage.formulario(parametros.dados)
+  testPage.formulario(parametros.dados);
 });
+
+Scenario("adicionar produto no carrinho", ({I, testPage}) => {
+  I.amOnPage("http://automationpractice.com/index.php");
+  testPage.buscarProduto(parametros.produto);
+  testPage.adicionarProdutoCarrinho();
+  testPage.modalConfirmacaoProduto();
+  testPage.finalizarPedido();
+})
 
 var parametros = {
   dados: {
@@ -29,4 +39,8 @@ var parametros = {
     mobilePhone: "112233445566",
     additionalInformation: 'espero que meu teste agrade vocês'
   },
+
+  produto: {
+    product: 'blouse'
+  }
 };
